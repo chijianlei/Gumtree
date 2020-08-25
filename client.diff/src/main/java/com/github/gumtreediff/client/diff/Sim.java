128d1108956606f209e93e5dcba544e7e9ed629f
chijianlei <chijianlei@gmail.com>
2020-08-25 15:28:21 +0800

    Add similarity calculation

---
+++ client.diff/src/main/java/com/github/gumtreediff/client/diff/Sim.java
@@ -0,0 +1,143 @@
+/*
+ * This file is part of GumTree.
+ *
+ * GumTree is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * GumTree is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with GumTree.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Copyright 2011-2015 Jean-Rémy Falleri <jr.falleri@gmail.com>
+ * Copyright 2011-2015 Floréal Morandat <florealm@gmail.com>
+ */
+
+package com.github.gumtreediff.client.diff;
+
+import com.github.gumtreediff.actions.Diff;
+import com.github.gumtreediff.actions.EditScript;
+import com.github.gumtreediff.client.Option;
+import com.github.gumtreediff.client.Register;
+import com.github.gumtreediff.io.ActionsIoUtils;
+import com.github.gumtreediff.matchers.MappingStore;
+import com.github.gumtreediff.tree.TreeContext;
+
+import java.io.IOException;
+import java.io.PrintStream;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Arrays;
+
+@Register(name = "sim", description = "Calculate the similarity",
+        options = AbstractDiffClient.Options.class)
+public class Sim extends AbstractDiffClient<Sim.Options> {
+    public Sim(String[] args) {
+        super(args);
+        if (!Files.isRegularFile(Paths.get(opts.src)))
+            throw new Option.OptionException("Source must be a file: " + opts.src, opts);
+        if (!Files.isRegularFile(Paths.get(opts.dst)))
+            throw new Option.OptionException("Destination must be a file: " + opts.dst, opts);
+
+        if (opts.format == null) {
+            opts.format = OutputFormat.TEXT;
+            if (opts.output != null) {
+                if (opts.output.endsWith(".json"))
+                    opts.format = OutputFormat.JSON;
+                else if (opts.output.endsWith(".xml"))
+                    opts.format = OutputFormat.XML;
+            }
+        }
+    }
+
+    public static class Options extends AbstractDiffClient.Options {
+        protected OutputFormat format;
+        protected String output;
+
+        @Override
+        public Option[] values() {
+            return Option.Context.addValue(super.values(),
+                    new Option("-f", String.format("format: %s", Arrays.toString(OutputFormat.values())), 1) {
+                        @Override
+                        protected void process(String name, String[] args) {
+                            try {
+                                format = OutputFormat.valueOf(args[0].toUpperCase());
+                            } catch (IllegalArgumentException e) {
+                                throw new Option.OptionException(String.format(
+                                        "No such format '%s', available formats are: %s\n",
+                                        args[0].toUpperCase(), Arrays.toString(OutputFormat.values())), e);
+                            }
+                        }
+                    },
+                    new Option("-o", "output file", 1) {
+                        @Override
+                        protected void process(String name, String[] args) {
+                            output = args[0];
+                        }
+                    }
+            );
+        }
+
+        @Override
+        void dump(PrintStream out) {
+            super.dump(out);
+            out.printf("format: %s\n", format);
+            out.printf("output file: %s\n", output == null ? "<stdout>" : output);
+        }
+    }
+
+    @Override
+    protected Options newOptions() {
+        return new Options();
+    }
+
+    @Override
+    public void run() throws Exception {
+        Diff diff = getDiff();
+        TreeContext srcT = diff.src;
+        TreeContext dstT = diff.dst;
+        int size_src = srcT.getRoot().getDescendants().size()+1;
+        int size_dst = dstT.getRoot().getDescendants().size()+1;
+        int diff_size = diff.mappings.size();
+        float sim = 1 - (float)(2*diff_size)/(size_src+size_dst);
+        System.out.println(sim);
+//        ActionsIoUtils.ActionSerializer serializer = opts.format.getSerializer(
+//                diff.src, diff.editScript, diff.mappings);
+//        if (opts.output == null)
+//            serializer.writeTo(System.out);
+//        else
+//            serializer.writeTo(opts.output);
+    }
+
+    enum OutputFormat {
+        TEXT {
+            @Override
+            ActionsIoUtils.ActionSerializer getSerializer(TreeContext sctx, EditScript actions, MappingStore mappings)
+                    throws IOException {
+                return ActionsIoUtils.toText(sctx, actions, mappings);
+            }
+        },
+        XML {
+            @Override
+            ActionsIoUtils.ActionSerializer getSerializer(TreeContext sctx, EditScript actions, MappingStore mappings)
+                    throws IOException {
+                return ActionsIoUtils.toXml(sctx, actions, mappings);
+            }
+        },
+        JSON {
+            @Override
+            ActionsIoUtils.ActionSerializer getSerializer(TreeContext sctx, EditScript actions, MappingStore mappings)
+                    throws IOException {
+                return ActionsIoUtils.toJson(sctx, actions, mappings);
+            }
+        };
+
+        abstract ActionsIoUtils.ActionSerializer getSerializer(TreeContext sctx, EditScript actions,
+                                                               MappingStore mappings) throws IOException;
+    }
+}
