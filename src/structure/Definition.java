c34d0a49184bba45a5cfb2899b5d27b2de083687
chijianlei <chijianlei@gmail.com>
2021-07-07 23:15:16 +0800

    Add missed files

---
+++ src/structure/Definition.java
@@ -0,0 +1,62 @@
+package structure;
+
+import java.util.List;
+
+import gumtreediff.tree.ITree;
+
+public class Definition {
+
+	private int defLabelID;
+	private ITree root;
+	private String type;
+	private String varName;
+	private ITree block;
+	private List<ITree> parBlocks;
+	private List<ITree> parList;
+	
+	public int getDefLabelID() {
+		return defLabelID;
+	}
+	public ITree getRoot() {
+		return root;
+	}
+	public String getType() {
+		return type;
+	}
+	public String getVarName() {
+		return varName;
+	}
+	public ITree getBlock() {
+		return block;
+	}
+	public List<ITree> getParBlocks() {
+		return parBlocks;
+	}
+	public List<ITree> getParList() {
+		return parList;
+	}
+	public void setDefLabelID(int defLabelID) {
+		this.defLabelID = defLabelID;
+	}
+	public void setRoot(ITree root) {
+		this.root = root;
+	}
+	public void setType(String type) {
+		this.type = type;
+	}
+	public void setVarName(String varName) {
+		this.varName = varName;
+	}
+	public void setBlock(ITree block) {
+		this.block = block;
+	}
+	public void setParBlocks(List<ITree> parBlocks) {
+		this.parBlocks = parBlocks;
+	}
+	public void setParList(List<ITree> parList) {
+		this.parList = parList;
+	}
+	
+
+	
+}
